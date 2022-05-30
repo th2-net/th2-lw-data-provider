8d21cd71167b4777d5359869f315f1d21088f4ff
oleg.smirnov <oleg.smirnov@exactprosystems.com>
2022-05-30 19:37:35 +0400

    [TH2-3597] Skip buffering if no sorting

--- src/main/kotlin/com/exactpro/th2/lwdataprovider/db/CradleMessageExtractor.kt
+++ src/main/kotlin/com/exactpro/th2/lwdataprovider/db/CradleMessageExtractor.kt
@@ -230,7 +230,7 @@ class CradleMessageExtractor(configuration: Configuration, private val cradleMan
                 requestContext.streamInfo.registerMessage(buffer.last.id)
                 tryDrain(group, buffer, detailsBuffer, batchBuilder, sort, requestContext)
             } else {
-                generateSequence { if (remaining.peek()?.timestampLess(currentBatch) == true) remaining.poll() else null }.toCollection(buffer)
+                generateSequence { if (!sort || remaining.peek()?.timestampLess(currentBatch) == true) remaining.poll() else null }.toCollection(buffer)
 
                 val messageCount = prev.messageCount
                 val prevRemaining = remaining.size
@@ -238,7 +238,7 @@ class CradleMessageExtractor(configuration: Configuration, private val cradleMan
                     if (needFiltration && !msg.inRange()) {
                         return@forEachIndexed
                     }
-                    if (msg.timestampLess(currentBatch)) {
+                    if (!sort || msg.timestampLess(currentBatch)) {
                         buffer += msg
                     } else {
                         check(remaining.size < groupBufferSize) {
