8ae92fe7a9d762209963a2205bf87d3e36dbea3b
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2025-08-21 12:17:44 +0400

    Added responseBufferSize to configuration

--- app/src/main/kotlin/com/exactpro/th2/lwdataprovider/http/util/JsonStreamingUtil.kt
+++ app/src/main/kotlin/com/exactpro/th2/lwdataprovider/http/util/JsonStreamingUtil.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 Exactpro (Exactpro Systems Limited)
+ * Copyright 2023-2025 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,6 +41,7 @@ fun writeJsonStream(
     dataMeasurement: DataMeasurement,
     logger: KLogger,
     progressListener: ProgressListener = DEFAULT_PROCESS_LISTENER,
+    bufferSize: Int = DEFAULT_BUFFER_SIZE
 ) {
     progressListener.onStart()
 
@@ -59,7 +60,7 @@ fun writeJsonStream(
         }
     }
 
-    val output = ctx.res().outputStream.buffered()
+    val output = ctx.res().outputStream.buffered(bufferSize)
     try {
         do {
             dataMeasurement.start("process_sse_event").use {
