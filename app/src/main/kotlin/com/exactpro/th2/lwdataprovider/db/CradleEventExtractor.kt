2ddeeef57bc325e7698a45fd44effa5ee28ffb6f
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2025-08-29 16:02:08 +0400

    Revert "Iterate events (with measure) without side effects"
    This reverts commit a2246521d31a93c629eb6c95e9d8c026c83b309b.

--- app/src/main/kotlin/com/exactpro/th2/lwdataprovider/db/CradleEventExtractor.kt
+++ app/src/main/kotlin/com/exactpro/th2/lwdataprovider/db/CradleEventExtractor.kt
@@ -206,18 +206,11 @@ class CradleEventExtractor(
             }
         }
 
-        for (testEvent in (measure("init_request") { storage.getTestEvents(cradleFilter) }).asIterableWithMeasurements(
-            "event",
-            dataMeasurement
-        )) {
-            // do noting
+        val testEvents = measure("init_request") { storage.getTestEvents(cradleFilter) }
+        processEvents(testEvents.asIterableWithMeasurements("event", dataMeasurement), sink, counter) { event ->
+            compareStart(event) && compareEnd(event)
+                    && filter.match(event)
         }
-
-//        val testEvents = measure("init_request") { storage.getTestEvents(cradleFilter) }
-//        processEvents(testEvents.asIterableWithMeasurements("event", dataMeasurement), sink, counter) { event ->
-//            compareStart(event) && compareEnd(event)
-//                    && filter.match(event)
-//        }
         logger.info { "Events for this period loaded. Count: $counter. Time ${System.currentTimeMillis() - startTime} ms" }
         sink.canceled?.apply {
             logger.info { "Loading events stopped: $message" }
